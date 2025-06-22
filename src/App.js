333bc47e7231844b219a28a36e3d639837c4ac70
Zain Farooqi <68056936+ZFierce@users.noreply.github.com>
2025-06-22 22:21:56 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,25 @@
 import React from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+// import SearchPage from "./pages/SearchPage"; // add more routes as needed
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          {/* <Route path="/search" element={<SearchPage />} /> */}
+        </Routes>
+      </Router>
+    </ChakraProvider>
+  );
+}
+
+export default App;
+
+import React from "react";
 import {
   ChakraProvider,
   Box,
